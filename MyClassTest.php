70dd19e1c258f56bc33d65825442083769035b44
Raymond Byczko <raymondbyczko@att.net>
2012-02-27 21:42:02 -0500

    RByczko self; spec test class file

---
+++ MyClassTest.php
@@ -0,0 +1,13 @@
+<?php
+	class MyClassTest extends PHPUnit_Framework_TestCase
+	{
+		public function testIt()
+		{
+			$this->assertEquals(0,1);
+		}
+		public function testThat()
+		{
+			$this->assertEquals(1,1);
+		}
+	}
+?>
